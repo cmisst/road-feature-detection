d961dc6d4b811570c8408c51396190b56629fcd4
Guangting Yu <yugtmath@umich.edu>
2019-08-02 00:35:48 -0400

    stack resized RGB images

--- dataset-label-maker.py
+++ dataset-label-maker.py
@@ -7,7 +7,7 @@ labels = []
 dataset = []
 
 dimension = (224, 224)
-input_path = './data/CNN_static_data/Train_augmented/'
+input_path = './data/CNN_static_data/Test/'
 output_name = input_path.split('/')[-2]
 if os.path.isfile(output_name + '-dataset.npy'):
     warnings.warn(output_name + '-dataset.npy already exists')
@@ -15,7 +15,8 @@ if os.path.isfile(output_name + '-dataset.npy'):
 for image_name in os.listdir(input_path):
     labels.append(int(image_name[-8], 2))
     image = Image.open(input_path + image_name)
-    dataset.append(np.reshape(image.resize(dimension).getdata(), dimension))
+    image = np.asarray(image.resize(dimension).convert('RGB'), dtype=np.uint8)
+    dataset.append(image)
 
 np.save(output_name + '-dataset.npy', dataset)
 np.save(output_name + '-labels.npy', labels)
