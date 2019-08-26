0517c265aa955e65f59e8aee6204ed8a88ed437f
Guangting Yu <yugtmath@gl-login1.arc-ts.umich.edu>
2019-08-26 19:55:09 -0400

    balanced dataset test result: not effective

--- CommonModel.py
+++ CommonModel.py
@@ -143,11 +143,16 @@ class CommonModel():
 def main():
     m = CommonModel()
     m.set_data_transform()
-    m.train(label_pos=-5, batch=50,epochs=10)
-    m.test(label_pos=-5, batch=700)
-    np.savetxt('predict_median.csv', 
-        m.predict(batch=1000, path='/scratch/engin_root/engin/yugtmath/Unlabeled_Satellite_IMG/'),
-        fmt='%d', delimiter=',')
+    m.train(label_pos=-5, batch=50, epochs=10, path='/scratch/engin_root/engin/yugtmath/Median_Satellite_View/train/')
+    m.test(label_pos=-5, batch=200, path='/scratch/engin_root/engin/yugtmath/Median_Satellite_View/test/')
+    m.test(label_pos=-5, batch=200, path='/scratch/engin_root/engin/yugtmath/Median_Satellite_View/Remainder1/')
+
+    m.train(label_pos=-8, batch=50, epochs=10, path='/scratch/engin_root/engin/yugtmath/Crosswalk_Satellite_View/train/')
+    m.test(label_pos=-8, batch=200, path='/scratch/engin_root/engin/yugtmath/Crosswalk_Satellite_View/test/')
+    m.test(label_pos=-8, batch=200, path='/scratch/engin_root/engin/yugtmath/Crosswalk_Satellite_View/Remainder0/')
+    # np.savetxt('predict_median.csv', 
+    #     m.predict(batch=1000, path='/scratch/engin_root/engin/yugtmath/Unlabeled_Satellite_IMG/'),
+    #     fmt='%d', delimiter=',')
     return
 
 
