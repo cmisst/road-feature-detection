00c4abe5a58f0ae36a7f5d409e2628005fc9a0f0
Guangting Yu <yugtmath@umich.edu>
2019-09-08 03:42:41 -0400

    analyze yolo output traffic lights and stop signs

--- yolo_output.py
+++ yolo_output.py
@@ -1,9 +1,19 @@
 def analyze_yolo(lines):
-    print(lines)
-    return 0
+    assert(len(lines)>=2)
+    assert(lines.pop(-1)=='Enter Image Path: ')
+    lines.pop(0)
+    for i in range(len(lines)):
+        lines[i] = lines[i].split(sep=':', maxsplit=1)[0]
+    d = dict.fromkeys(lines)
+    if 'traffic light' in d:
+        return 1
+    elif 'stop sign' in d:
+        return 2
+    else:
+        return 0
 
 if __name__ == "__main__":
     for i in range(4):
         f = open('tmpout'+str(i)+'.log', 'r')
-        analyze_yolo(f.readlines())
+        print(analyze_yolo(f.readlines()))
         f.close()
